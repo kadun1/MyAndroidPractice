979bf3b0a15480661f7dc07c8c3d647044b84e94
kadun1 <kadunjj@gmail.com>
2022-06-09 00:07:46 +0900

    안드로이드실습 Retrofit2 사용해보기

---
+++ app/src/main/java/com/example/myandroidpractice/model/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package com.example.myandroidpractice.model;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
