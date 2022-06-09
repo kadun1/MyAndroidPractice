64c3fd4bf41ef15ebc507c261892ec7d74fde484
kadun1 <kadunjj@gmail.com>
2022-06-10 00:00:16 +0900

    안드로이드실습 스레드 예제 연습

--- app/src/main/java/com/example/myandroidpractice/MainActivity.java
+++ app/src/main/java/com/example/myandroidpractice/MainActivity.java
@@ -3,66 +3,41 @@ package com.example.myandroidpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.example.myandroidpractice.model.JsonPlaceHolderApi;
-import com.example.myandroidpractice.model.Post;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewResult;
+    private static final int PROGRESS = 0x1;
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+    int i = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
 
-        textViewResult = findViewById(R.id.text_view_result);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
-
-        call.enqueue(new Callback<List<Post>>() {
+        //
+        new Thread(new Runnable() {
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if (!response.isSuccessful()) {
-                    textViewResult.setText("Code: " + response.code());
-                    return;
-                }
+            public void run() {
+                while (mProgressStatus < 100) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    mProgressStatus = i++;
 
-                List<Post> posts = response.body();
-
-                for (Post post : posts) {
-                    String content = "";
-                    content += "ID: " + post.getId() + "\n";
-                    content += "User ID: " + post.getUserId() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";
-                    content += "Text: " + post.getText() + "\n\n";
-
-                    textViewResult.append(content);
-
+                    //
+                    mProgress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus);
+                        }
+                    });
                 }
             }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-
-                textViewResult.setText(t.getMessage());
-            }
-        });
+        }).start();
     }
-
 }
